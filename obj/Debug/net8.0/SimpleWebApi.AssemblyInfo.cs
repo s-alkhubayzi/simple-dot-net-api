04b92169b49d26a12f2d9760f8c75cba2aef9c78
s-alkhubayzi <sultan.alkhubayzi@devoteam.com>
2024-12-25 12:45:07 +0300

    Untrack files in .gitignore

--- obj/Debug/net8.0/SimpleWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a864ef447ad8d502d8ae7bde955663b5872cafee")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
